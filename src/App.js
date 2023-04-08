49ec7aefcc6f2e97b0000225aa3f570db78b3c86
onemmnuel4christ <onemmanuel4christ@gmail.com>
2023-04-09 00:34:16 +0100

    content add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
-import Test from "./pages/Test";
 import Admin from "./pages/Admin";
+import ErrorPage from "./pages/404";
 function App() {
   return (
     <div className="max-w-[1512px] mx-auto bg-[#FFFFFFA3] min-h-[100vh]">
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/test" element={<Test />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className='min-h-[85vh] flex justify-self-center items-center w-full'>
+      <p className='text-6xl font-bold text-center w-full'>Sorry page not found </p>
+    </div>
+  );
+}
+
+export default ErrorPage
